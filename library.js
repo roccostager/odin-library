d22ba80aa88702d5a86a2b6904b7f11c541690d5
roccostager <99860439+roccostager@users.noreply.github.com>
2025-12-26 16:44:52 +0100

    Completed functionality

--- library.js
+++ library.js
@@ -18,11 +18,16 @@ openModalButton.addEventListener('click', () => {
     modal.classList.add('visible');
 })
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('submit', e => {
     e.preventDefault();
+    modal.classList.remove('visible');
+
     const data = new FormData(e.target);
     const objectifiedData = storeInputData(data);
     myLibrary.push(objectifiedData);
+
+    displayData();
+    form.reset();
 })
 
 function storeInputData(data) {
@@ -33,4 +38,19 @@ function storeInputData(data) {
         data.get('read') === 'on',
     );
     return newBook;
+}
+
+function displayData() {
+    tableDisplay.innerHTML = '';
+
+    for (let i = 0; i < myLibrary.length; i++) {
+        const row = document.createElement('tr');
+        tableDisplay.append(row);
+
+        Object.values(myLibrary[i]).forEach(value => {
+            const td = document.createElement('td');
+            td.innerText = value;
+            row.append(td);
+        });
+    }
 }

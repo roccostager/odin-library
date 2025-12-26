95d95974204dfad3ce647357cc981104cfc96c64
roccostager <99860439+roccostager@users.noreply.github.com>
2025-12-26 15:00:28 +0100

    Created array and object constructor for book

---
+++ library.js
@@ -0,0 +1,12 @@
+const myLibrary = [];
+
+function Book(title, author, date, read) {
+    if (!new.target) throw Error('Use new operator to call this function');
+
+    this.title = title;
+    this.author = author;
+    this.date = date;
+    this.read = read;
+}
+
+const tableDisplay = document.getElementById('book-data')
